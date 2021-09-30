9c2e22e40a2b9aab2e451140831cc13c015bfd38
André Wagner <WagnerAndre@JohnDeere.com>
2021-09-30 09:21:38 -0300

    .

--- fortuna.js
+++ fortuna.js
@@ -1,12 +1,53 @@
+// TODO - make arrows work (ok)
+// TODO - arrows in mobile
+// TODO - show image in full screen
+// TODO - publish and calculate lighthouse
+
 const photoIndex = {};
 
-for (const photoSet of document.getElementsByClassName("image")) {
-    photoIndex[photoSet.className] = {
+function onClickRight(id) {
+    const p = photoIndex[id];
+
+    p.photos.item(p.index).style.display = 'none';
+    ++p.index;
+    p.photos.item(p.index).style.display = 'block';
+
+    if (p.index > 0)
+        p.arrowLeft.style.display = 'block';
+    if (p.index === p.photos.length - 1)
+        p.arrowRight.style.display = 'none';
+}
+
+function onClickLeft(id) {
+    const p = photoIndex[id];
+
+    p.photos.item(p.index).style.display = 'none';
+    --p.index;
+    p.photos.item(p.index).style.display = 'block';
+
+    if (p.index < p.photos.length - 1)
+        p.arrowRight.style.display = 'block';
+    if (p.index === 0)
+        p.arrowLeft.style.display = 'none';
+}
+
+for (const block of document.getElementsByClassName('block')) {
+    const id = block.id;
+
+    photoIndex[id] = {
         index: 0,
-        photos: photoSet.children
+        photos: block.getElementsByClassName('image').item(0).children,
+        arrowLeft: block.getElementsByClassName('arrow-left').item(0),
+        arrowRight: block.getElementsByClassName('arrow-right').item(0),
     };
 
-    for (let i = 1; i < photoSet.children.length; ++i) {
-        photoSet.children[i].style.display = 'none';
+    if (photoIndex[id].arrowRight) {
+        photoIndex[id].arrowRight.style.display = 'block';
+        photoIndex[id].arrowRight.addEventListener('click', () => onClickRight(id));
     }
+
+    if (photoIndex[id].arrowLeft)
+        photoIndex[id].arrowLeft.addEventListener('click', () => onClickLeft(id));
 }
+
+console.log(photoIndex);
